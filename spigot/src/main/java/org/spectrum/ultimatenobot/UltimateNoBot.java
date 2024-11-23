d1ac62de7ff0ee63c232c58ed77e96305c89dbb5
spectrum <105482424+TheSpectrumProject@users.noreply.github.com>
2024-11-23 21:34:27 +0800

    Update UltimateNoBot.java

--- spigot/src/main/java/org/spectrum/ultimatenobot/UltimateNoBot.java
+++ spigot/src/main/java/org/spectrum/ultimatenobot/UltimateNoBot.java
@@ -7,12 +7,8 @@ public class UltimateNoBot extends JavaPlugin {
     @Override
     public void onEnable() {
         saveDefaultConfig();  // 如果没有配置文件，则创建一个默认的config.yml
-        // 创建IPLoginLimiter实例并注册事件
+        getServer().getPluginManager().registerEvents(new PlayerLoginCheck(this), this);
         getServer().getPluginManager().registerEvents(new IPLoginLimiter(this), this);
-        // 注册玩家加入事件监听器
-        getServer().getPluginManager().registerEvents(new GameJoinCheck(this), this);
-
-        getServer().getPluginManager().registerEvents(new LoginInterval(this), this);
         getLogger().info("IPLoginLimitPlugin 已启用！");
     }
 
